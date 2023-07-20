00706039f3399cb0e263172bcd3cc78ff139bf15
Lynn Myat Aung <66087784+hyper-lynn@users.noreply.github.com>
2023-07-21 03:57:17 +0630

    Update and rename mydb.py to create_db.py

--- dcrm/create_db.py
+++ dcrm/create_db.py
@@ -1,17 +1,12 @@
 import mysql.connector
 
-
-
 dataBase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
     password = 'root',
 )
 
-#prepare the cursor object
 cursorObject = dataBase.cursor()
-
-#create database 
 cursorObject.execute("CREATE DATABASE blackcat")
 
 print("ALL done")
